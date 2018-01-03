11430bbd0e0ed52b1df12099f3ac5f81eb19cc7b
dodgerogers <dodgerogers@hotmail.com>
2018-01-02 19:30:02 -0500

    TDNA-183 | Signup form validations

---
+++ test/views/semantic-ui/EmailSignUpForm-test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import TestUtils from "react-addons-test-utils";
+import sinon from "sinon";
+import { expect } from "chai";
+
+describe('EmailSignUpForm', () => {
+  it('should render correctly');
+});
