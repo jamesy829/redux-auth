5bd580dff861895f7ed3b5591e03f260f3a71795
James Huang <jamesy.huang@hotmail.com>
2018-01-24 11:13:34 -0500

    Build latest release after rebase from carolhealth/redux-auth fork

--- semantic-ui-theme.js
+++ semantic-ui-theme.js
@@ -3130,8 +3130,31 @@
 
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-	var ExtendableError = function (_Error) {
-	  _inherits(ExtendableError, _Error);
+	function _extendableBuiltin(cls) {
+	  function ExtendableBuiltin() {
+	    cls.apply(this, arguments);
+	  }
+
+	  ExtendableBuiltin.prototype = Object.create(cls.prototype, {
+	    constructor: {
+	      value: cls,
+	      enumerable: false,
+	      writable: true,
+	      configurable: true
+	    }
+	  });
+
+	  if (Object.setPrototypeOf) {
+	    Object.setPrototypeOf(ExtendableBuiltin, cls);
+	  } else {
+	    ExtendableBuiltin.__proto__ = cls;
+	  }
+
+	  return ExtendableBuiltin;
+	}
+
+	var ExtendableError = function (_extendableBuiltin2) {
+	  _inherits(ExtendableError, _extendableBuiltin2);
 
 	  function ExtendableError() {
 	    var message = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
@@ -3170,7 +3193,7 @@
 	  }
 
 	  return ExtendableError;
-	}(Error);
+	}(_extendableBuiltin(Error));
 
 	exports.default = ExtendableError;
 	module.exports = exports['default'];
